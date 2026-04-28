31becc9ed4b2e5476c01344502b40b76b30b06ca
hector jose <hector2871@gmail.com>
2026-04-28 16:36:03 +0200

    fix: cambios en el avion.api

--- src/api/avion/avion.api.ts
+++ src/api/avion/avion.api.ts
@@ -2,16 +2,20 @@ import express from 'express'
 
 export async function InfoAvionFunction( req:express.Request, res:express.Response ) {
 
-        const respuesta:string = "Lista de productos en el inventario";
+        const respuesta:string = "informacion del avion solicitado";
 
     return res.status(200).send(respuesta)
 }
 
 export async function ObtenerInfoAvionFunction( req:express.Request, res:express.Response ) {
 const respuesta = {
-    mensaje:"Usted ha agregado un nuevo producto al inventario. Estos son sus datos",
+    mensaje:"Usted ha solicitado informacion del avion",
     almacen:"Almacen principal",
     fechaIngreso:"17PM",
+    infoavion:"737 MAX",
+    tipo:"Fuselaje estrecho",
+    Usoprincipal:"Rutas cortas/medias",
+
     datosProducto:req.body
 }
 
