12b5dfe02c1f376459ff1d5d0bb33178995fdfd1
hector jose <hector2871@gmail.com>
2026-05-04 16:30:25 +0200

    fix: areglado fallo de versionamiento

--- src/api/DtoCalculadora/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { ObtenerResultadoFunction, CalcularOperacionFunction } from './calculadora.api';
-
-const DtoCalculadora = Router()
-
-DtoCalculadora.get('/calcular',   ObtenerResultadoFunction)
-DtoCalculadora.post('/calcular',  CalcularOperacionFunction)
-
-export default DtoCalculadora
