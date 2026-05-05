e7612b75457afb2f93598bcd141aaef87162ba55
hector jose <hector2871@gmail.com>
2026-05-05 14:51:45 +0200

    new: añadidos al routes principal

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { Router } from 'express'
-import DtoTraducciones from './DtoTraducciones/routes.DtoTraducciones'
-import DtoCalculadora from './DtoCalculadora/routes.DtoCalculadora'
-  
- //hola
- const api = Router()
-
-api.use( '/DtoTraducciones',  DtoTraducciones)
-     
-api.use( '/DtoCalculadora',  DtoCalculadora)
-
-            
-  
-=======
  //Aquí encontramos carpetas organizada por módulos.
  import { Router } from 'express'
 
@@ -23,7 +8,7 @@ import DtoCalculadora from './DtoCalculadora/routes';
 import DtoRecursosHumanos from './Dto.RecursosHumanos/routes.DtoRecursosHumanos';
 import DtoInventario from './DtoInventario/routes';
 import avion from './avion/routes';
-
+import moteros from './ClubMoteros/routes';
 
 // Se define de 2 maneras según el enrutamiento de la aplicación 
 const api = Router()
@@ -35,6 +20,5 @@ api.use('/DtoCalculadora', DtoCalculadora);
 api.use('/DtoRecursosHumanos', DtoRecursosHumanos)
 api.use('/DtoInventario', DtoInventario)
 api.use('/avion', avion)
->>>>>>> 31becc9ed4b2e5476c01344502b40b76b30b06ca
-             
+api.use('/moteros', moteros)             
 export default api
