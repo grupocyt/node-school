698a0fe1bb37cf6f15aae5526aac9da8cf85a714
hector jose <hector2871@gmail.com>
2026-05-12 19:11:18 +0200

    NEW: Añadir controladores, rutas y apis

--- src/api/DtoMecanica/routes.ts
+++ src/api/DtoMecanica/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { ObtenerInfoTallerFunction } from './mecanica.api';
+import { obtenercolordeltaller, obtenerinfoduenotaller, ObtenerInfoTallerFunction, obtenerlocalderecho, obtenerlocalizquierdo } from './mecanica.api';
  
 
 const mecanica = Router()
 
-mecanica.get('/moteros',           ObtenerInfoTallerFunction)
+mecanica.get('/ObtenerInfoTallerFunction',           ObtenerInfoTallerFunction)
+mecanica.get(`/obtenerinfoduenotaller`,              obtenerinfoduenotaller)
+mecanica.get(`/obtenerlocalizquierdo`,               obtenerlocalizquierdo )
+mecanica.get(`/obtenerlocalderecho`,                 obtenerlocalderecho )
+mecanica.get(`/obtenercolordeltaller`,               obtenercolordeltaller)
 
 export default mecanica
