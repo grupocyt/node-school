5bb42093e8ba93be803d1cec3d9f34dc819a2f3b
hector jose <hector2871@gmail.com>
2026-05-18 16:08:56 +0200

    new: añadiendo rutas que se estropearon

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import avion from './avion/routes';
+import calculadora from './calculadora/routes';
+import camarero from './camarero/routes';
+
 
 const api = Router()
  
 // 2 forma "rutas": utilizamos las palabras reservadas GET, POST, PATCH, DELETE, PUT, HEADER para conectar con la funcion
 api.use('/avion', avion)
-  
+api.use('/calculadora', calculadora)
+api.use('/camarero', camarero)
+
 export default api
