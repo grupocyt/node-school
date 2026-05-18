46e132cdabf179fd41eff6241536b66a99ce9aa7
hector jose <hector2871@gmail.com>
2026-05-18 16:35:40 +0200

    new: añadido nuevo departamento autobus

---
+++ src/controllers/autobus/autobus.controller.ts
@@ -0,0 +1,24 @@
+export class autobuscontroller {
+
+        async obtenerinfoautobus () : Promise<string> {
+        const respuesta:string = `Informacion del autobus`;
+        return respuesta;
+    }
+
+  async obtenerinfohorarios () : Promise<string> {
+        const respuesta:string = `Informacion del horario del autobus`;
+        return respuesta;
+    }
+
+  async obtenerinfopuestodelcamarero () : Promise<string> {
+        const respuesta:string = `Informacion del puesto del recorrido del autobus`;
+        return respuesta;
+    }
+
+     async obtenerinfoestadoautobus () : Promise<string> {
+        const respuesta:string = `Informacion del estado del autobus`;
+        return respuesta;
+    }
+
+}
+  
