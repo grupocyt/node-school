ef0c4a739049ed6041e1ed00bff30a26b54da805
hector jose <hector2871@gmail.com>
2026-05-19 21:26:47 +0200

    new: añadido controlador Shop

---
+++ src/controllers/shop/shop.controller.ts
@@ -0,0 +1,24 @@
+export class shopcontroller {
+
+        async ObtenerDirTienda () : Promise<string> {
+        const respuesta:string = `Informacion de la tienda`;
+        return respuesta;
+    }
+
+  async ObtenerInfoHorarioTienda () : Promise<string> {
+        const respuesta:string = `Informacion del horario de la tienda`;
+        return respuesta;
+    }
+
+  async ObtenerInfoProductos () : Promise<string> {
+        const respuesta:string = `Informacion de los productos de la tienda`;
+        return respuesta;
+    }
+
+     async ObtenerInfoPago () : Promise<string> {
+        const respuesta:string = `Informacion del metodo de pago`;
+        return respuesta;
+    }
+
+}
+  
