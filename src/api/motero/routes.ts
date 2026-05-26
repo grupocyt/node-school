e77ef4dbb004889d93b5cca6b6d65cade8acb345
hector jose <hector2871@gmail.com>
2026-05-26 17:05:15 +0200

    feat(motero):implementacion clase motero api

--- src/api/motero/routes.ts
+++ src/api/motero/routes.ts
@@ -1,18 +1,14 @@
-import { Router } from 'express';
-import { 
-    RegistrarMoteroFunction,
-    SacarMoteroDelGrupoFunction,
-    ObtenerInfoMoteroFunction,
-    RepostarMotoFunction,
-    CambiarLlantasFunction
-} from './motero.api';
+import { Request, Response, Router } from 'express';
+import { MoteroApi } from './motero.api';
 
 const motero = Router()
 
-motero.get('/moteros',              ObtenerInfoMoteroFunction)
-motero.post('/moteros',             RegistrarMoteroFunction)
-motero.delete('/moteros',           SacarMoteroDelGrupoFunction)
-motero.patch('/moteros/repostar',   RepostarMotoFunction)
-motero.patch('/moteros/llantas',    CambiarLlantasFunction)
+const api = new MoteroApi()
+
+motero.get('/moteros',                  (req:Request, res:Response) => api.registrarMotero(req,res))
+motero.post('/moteros',                 (req:Request, res:Response) => api.sacarMoteroDelGrupo(req,res))
+motero.delete('/moteros',               (req:Request, res:Response) => api.obtenerInfoMotero(req,res))
+motero.patch('/moteros/repostar',       (req:Request, res:Response) => api.repostarMoto(req,res))
+motero.patch('/moteros/llantas',        (req:Request, res:Response) => api.cambiarLlantas(req,res))
 
 export default motero
