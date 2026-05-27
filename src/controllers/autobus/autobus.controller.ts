11db53c269d3cc3ab6f4455d79a3a91a6d267cc4
hector jose <hector2871@gmail.com>
2026-05-27 19:00:56 +0200

    feat(autobus):implementacion clase motero api

--- src/controllers/autobus/autobus.controller.ts
+++ src/controllers/autobus/autobus.controller.ts
@@ -1,21 +1,21 @@
-export class autobuscontroller {
+export class AutobusController {
 
-        async ObtenerInfoAutobus () : Promise<string> {
+        async obtenerInfoAutobus () : Promise<string> {
         const respuesta:string = `Informacion del autobus`;
         return respuesta;
     }
 
-  async ObtenerInfoHorarios () : Promise<string> {
+  async obtenerInfoHorarios () : Promise<string> {
         const respuesta:string = `Informacion del horario del autobus`;
         return respuesta;
     }
 
-  async ObtenerInfoRecorrido () : Promise<string> {
+  async obtenerInfoRecorrido () : Promise<string> {
         const respuesta:string = `Informacion del puesto del recorrido del autobus`;
         return respuesta;
     }
 
-     async ObtenerInfoEstadoAutobus () : Promise<string> {
+     async obtenerInfoEstadoAutobus () : Promise<string> {
         const respuesta:string = `Informacion del estado del autobus`;
         return respuesta;
     }
