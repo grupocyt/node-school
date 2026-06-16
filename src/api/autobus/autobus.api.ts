1f9cca8454af07dc6baef9a1eed2ffb61f87fc20
hector jose <hector2871@gmail.com>
2026-06-16 16:20:47 +0200

    fix:areglos en funcionalidad

--- src/api/autobus/autobus.api.ts
+++ src/api/autobus/autobus.api.ts
@@ -1,22 +1,22 @@
 import express from 'express'
 import { autobuscontroller } from '../../controllers/autobus/autobus.controller'
 
-export async function obtenerinfoautobusfunction ( req:express.Request, res:express.Response ) {
-    const controlador =  await new autobuscontroller().obtenerinfoautobus()
+export async function ObtenerInfoAutobusFunction ( req:express.Request, res:express.Response ) {
+    const controlador =  await new autobuscontroller().ObtenerInfoAutobus()
     return res.status(200).send(controlador)
 }
 
-export async function obtenerinfohorariosfunction ( req:express.Request, res:express.Response ) {
-    const controlador = await new autobuscontroller().obtenerinfohorarios()
+export async function ObtenerInfoHorariosFunction ( req:express.Request, res:express.Response ) {
+    const controlador = await new autobuscontroller().ObtenerInfoHorarios()
     return res.status(200).send(controlador)
 }
 
-export async function obtenerinfoestadoautobusfunction( req:express.Request, res:express.Response ) {
-    const controlador = await new autobuscontroller().obtenerinfoestadoautobus()
+export async function ObtenerInfoEstadoAutobusFunction( req:express.Request, res:express.Response ) {
+    const controlador = await new autobuscontroller().ObtenerInfoEstadoAutobus()
     return res.status(200).send(controlador)
 }
 
-export async function obtenerinforecorridofunction( req:express.Request, res:express.Response ) {
-    const controlador = await new autobuscontroller().obtenerinforecorrido()
+export async function ObtenerInfoRecorridoFunction( req:express.Request, res:express.Response ) {
+    const controlador = await new autobuscontroller().ObtenerInfoRecorrido()
     return res.status(200).send(controlador)
 }
--- src/api/mecanica/mecanica.api.ts
+++ src/api/mecanica/mecanica.api.ts
@@ -2,26 +2,26 @@ import express from 'express'
 import { MecanicaController } from '../../controllers/Mecanica/mecanica.controller'
 
 export async function ObtenerInfoTallerFunction( req:express.Request, res:express.Response ) {
-    const controlador = await new MecanicaController().obtenerinfotaller()
+    const controlador = await new MecanicaController().ObtenerInfoTallerFunction()
     return res.status(200).send(controlador)
 }
 
 export async function obtenerinfoduenotaller( req:express.Request, res:express.Response ) {
-    const controlador = await new MecanicaController().obtenerinfoduenotaller()
+    const controlador = await new MecanicaController().ObtenerInfoDuenoTallerFunction()
     return res.status(200).send(controlador)
 }
 
 export async function obtenerlocalizquierdo(req:express.Request, res:express.Response ){
-    const controlador = await new MecanicaController().obtenerlocalizquierdo()
+    const controlador = await new MecanicaController().ObtenerLocalIzquierdoFunction()
     return res.status(200).send(controlador) 
 }
 
 export async function obtenerlocalderecho(req:express.Request, res:express.Response ){
-    const controlador = await new MecanicaController().obtenerlocalderecho()
+    const controlador = await new MecanicaController().ObtenerLocalDerechoFunction()
     return res.status(200).send(controlador)
 }
 
 export async function obtenercolordeltaller(req:express.Request, res:express.Response) {
-    const controlador = await new MecanicaController().obtenercolordeltaller()
+    const controlador = await new MecanicaController().ObtenerColorDelTallerFunction()
     return res.status(200).send(controlador)
 }
