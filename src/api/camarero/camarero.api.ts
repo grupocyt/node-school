5f88094bdeeac16f261769549332964d94fed276
hector jose <hector2871@gmail.com>
2026-06-16 16:30:03 +0200

    fix:areglos en funcionalidad

--- src/api/camarero/camarero.api.ts
+++ src/api/camarero/camarero.api.ts
@@ -2,12 +2,12 @@ import express from 'express'
 import { camareroscontroller } from '../../controllers/Camareros/camareros.controller'
 
 export async function obtenerinfocamarero( req:express.Request, res:express.Response ) {
-    const controlador = await new camareroscontroller().obtenerinfocamarero()
+    const controlador = await new camareroscontroller().ObtenerInfoCamareroFunction()
     return res.status(200).send(controlador)
 }
 
 export async function obtenerinfohorariocamarero( req:express.Request, res:express.Response ) {
-    const controlador = await new camareroscontroller().obtenerinfohorariocamarero()
+    const controlador = await new camareroscontroller().ObtenerInfoHorarioCamareroFunction()
     return res.status(200).send(controlador)
 }
 
